7ad6ad69b1b4275ce5c21f8523a4a2d57d46248e
Taiyo <taiyoperson@gmail.com>
2025-02-02 05:31:02 +1300

    feat: Configure webpack merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,33 +1,6 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html"
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader"
-            },
-            {
-                test: /\.(jpg|jpeg|png|svg|gif)$/i,
-                type: "asset/resource"
-            }
-        ]
-    }
-}
+module.exports = merge(common, {
+    mode: "development"
+});
