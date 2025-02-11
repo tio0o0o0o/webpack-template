365c4feee0bae2624493c2f052375bf6bf468e52
Taiyo <taiyoperson@gmail.com>
2025-02-11 17:15:48 +1300

    feat: Add source map

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,41 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { watchFile } = require("fs");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html"
-        })
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(ttf|otf|woff|woff2)$/i,
+        type: "asset/resource",
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader"
-            },
-            {
-                test: /\.(jpg|jpeg|png|svg|gif)$/i,
-                type: "asset/resource"
-            }
-        ]
-    }
-}
+  },
+};
